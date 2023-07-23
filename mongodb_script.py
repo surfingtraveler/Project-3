2af7b37ec9dd03a4e403ff1a33dd49606cb3cd68
surfingtraveler <cabomurphy24@gmail.com>
2023-07-23 18:30:18 -0500

    updated file

--- mongodb_script.py
+++ mongodb_script.py
@@ -1,4 +1,4 @@
-import json
+import csv
 from pymongo import MongoClient
 
 # Establish the MongoDB connection
@@ -6,40 +6,36 @@ def get_mongo_connection():
     client = MongoClient('mongodb://localhost:27017/')
     return client
 
-# Connect to the 'sharks' database and 'attacks' collection
+# Connect to database and collection
 def get_sharks_collection():
     client = get_mongo_connection()
     db = client.sharks_db  
     collection = db.attacks
     return collection
 
-# Load data from JSON file
-def load_data_from_json(json_file):
-    with open(json_file, "r") as file:
-        data = json.load(file)
-    return data
-
-# Replace null values with "Unknown" and insert data into the mongo database
-def insert_data_from_json():
+# Clean/replace null values with "Unknown" and add to our database
+def insert_data_from_csv(csv_file):
     collection = get_sharks_collection()
-    json_data = load_data_from_json("global_shark_attacks.json")
 
-    for item in json_data:
-        for key, value in item.items():
-            if value is None:
-                item[key] = "Unknown"
+    with open(csv_file, "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            for key, value in row.items():
+                if value.strip() == "":
+                    row[key] = "Unknown"
 
-    # Insert data into the mongo database
-    if json_data:
-        result = collection.insert_many(json_data)
-        print(f"{len(result.inserted_ids)} valid data entries inserted into the database.")
-    else:
-        print("No valid data to insert.")
+                # Convert Latitude and Longitude fields to float
+                if key == "latitude" or key == "longitude":
+                    try:
+                        row[key] = float(value)
+                    except ValueError:
+                        row[key] = 0.0
+
+            # Insert data into our database
+            collection.insert_one(row)
+
+    print("Data from CSV successfuly inserted into the database.")
 
 if __name__ == "__main__":
-    insert_data_from_json()
-
-
-
-
-
+    csv_file = "global-shark-attack_updated_for_LAT_LONG.csv"
+    insert_data_from_csv(csv_file)
